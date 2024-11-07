4020ee6cd844aee177a22cc69e6f174cec450c02
mckeeba <mckeeba@appstate.edu>
2024-11-07 16:37:49 -0500

    integrated blacksmith's daughter and fixed dialogue placement issue

--- godot-game/scenes/daughter.gd
+++ godot-game/scenes/daughter.gd
@@ -11,7 +11,7 @@ func _on_body_entered(body):
 	# Enable interaction when the Player enters the NPC's interaction area
 	if body.name == "Player":
 		# Trigger the dialogue start() function in dialoguePlayer.gd
-		var dialogue = get_parent().get_node("Daughter/DialogueBlk")  # Adjust the path to your Dialogue node
+		var dialogue = get_parent().get_node("DaughterBlacksmith/DialogueDaught")  # Adjust the path to your Dialogue node
 		if dialogue:
 			dialogue.start()
 
